de85125e5ab2dad3bd8e2bc37e3907de01b90ede
Zi Yang <61411023+yzy98@users.noreply.github.com>
2025-05-23 18:20:33 +0800

    setup better-auth (#3)

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { signIn, signOut, useSession } from "@/lib/auth-client";
+
+const session = useSession();
+</script>
+
+<template>
+  <div>
+    <button
+      v-if="!session?.data" @click="() => signIn.social({
+        provider: 'github',
+      })"
+    >
+      Continue with GitHub
+    </button>
+    <div>
+      <pre>{{ session.data }}</pre>
+      <button v-if="session.data" @click="signOut()">
+        Sign out
+      </button>
+    </div>
+  </div>
+</template>
